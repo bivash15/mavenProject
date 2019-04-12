0d174891767b2e952231331c1dc4b4a82527579c
bivash15 <bivash15@gmail.com>
2019-04-12 16:04:15 +0530

    sop deleted

--- MVCAnno29-LoginApp-SFC1/src/main/java/com/nit/service/UserServiceImpl.java
+++ MVCAnno29-LoginApp-SFC1/src/main/java/com/nit/service/UserServiceImpl.java
@@ -16,7 +16,6 @@ public class UserServiceImpl implements UserService {
 	@Override
 	@Transactional(propagation=Propagation.REQUIRED,readOnly=true)
 	public String validate(UserDTO dto) {
-		System.out.println("This is validate() method");
 		UserBO bo=new UserBO();
 		//convert dto to bo
 		BeanUtils.copyProperties(dto, bo);
